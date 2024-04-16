39eaaa33e37cc55b844f9647cea35a292e97dd16
Robert Bamler <robert.bamler@uni-tuebingen.de>
2024-04-17 00:33:00 +0200

    Decode matrix & multiply vector on the fly
    Includes a rudimentary benchmark that starts from a dummy vector and
    then multiplies the same 1024x1024 matrix to it 10_000 times in
    sequence. Each iteration creates the lookup table from scratch, decodes
    decodes the matrix again, and multiplies it to the vector while decoding
    it. The time for loading the compressed matrix onto the GPU is not
    included in the benchmark.
    
    The benchmark takes about 8 seconds, i.e., 0.8 ms per decode+multiply
    cycle. Of this, the creation of the lookup table takes only about
    0.01ms, the rest is for decoding + multiplying.

--- decompress/main.js
+++ decompress/main.js
@@ -1,21 +1,108 @@
 async function run() {
     const canvas = document.getElementById("canvas");
     const gl = getGl(canvas);
-    const inputDataTexture = await createInputDataTexture(canvas, gl);
-    const lookupTableTexture = await createLookupTable(canvas, gl, inputDataTexture);
-    await decodeMatrix(canvas, gl, inputDataTexture, lookupTableTexture);
+    const vertexShader = compileShader(gl, "vertex-shader");
 
-    // const vertexShader = compileShader(gl, "vertex-shader");
-    // const fragmentShader = compileShader(gl, "fragment-shader");
-    // const program = createProgram(gl, vertexShader, fragmentShader);
-    // gl.useProgram(program);
-    // const num_vertices = initVertexBuffers(gl, program);
-    // gl.drawArrays(gl.TRIANGLES, 0, num_vertices);
+    const compressedMatrixTexture = await createInputDataTexture(gl);
+    const lookupTableTexture = createEmptyLookupTableTexture(gl);
+    let inputVectorTexture = createVectorTexture(gl);
+    let outputVectorTexture = createVectorTexture(gl);
+
+    const framebuffer = gl.createFramebuffer();
+    gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer);
+    gl.framebufferTexture2D( // TODO: maybe use gl.DRAW_FRAMEBUFFER for first argument
+        gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, outputVectorTexture, 0);
+
+    const createLookupTable = new GlProgram(gl, vertexShader, "create-lookup-shader", ["compressedMatrix"]);
+    const compressedMatrixVectorProduct = new GlProgram(
+        gl, vertexShader, "compressed-mat-vec-mul-shader", ["compressedMatrix", "lookupTable", "vector"]);
+
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    let start = performance.now();
+    for (let i = 0; i != 10_000; ++i) {
+        createLookupTable.run(gl, [compressedMatrixTexture], lookupTableTexture, 512, 8);
+        compressedMatrixVectorProduct.run(
+            gl, [compressedMatrixTexture, lookupTableTexture, inputVectorTexture],
+            outputVectorTexture, 512, 2);
+        let tmp = inputVectorTexture;
+        inputVectorTexture = outputVectorTexture;
+        outputVectorTexture = tmp;
+    }
+
+    let rawBuffer = new ArrayBuffer(512 * 2 * 4 * 4);
+    let output = new Int32Array(rawBuffer);
+    gl.readPixels(0, 0, 512, 2, gl.RGBA_INTEGER, gl.INT, output);
+    console.log(output.filter((_val, i) => i % 4 == 0));
+
+    await new Promise(resolve => setTimeout(resolve, 0));
+    let end = performance.now();
+    console.log('duration: ' + (end - start))
+
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    start = performance.now();
+    for (let i = 0; i != 10_000; ++i) {
+        createLookupTable.run(gl, [compressedMatrixTexture], lookupTableTexture, 512, 8);
+        compressedMatrixVectorProduct.run(
+            gl, [compressedMatrixTexture, lookupTableTexture, inputVectorTexture],
+            outputVectorTexture, 512, 2);
+        let tmp = inputVectorTexture;
+        inputVectorTexture = outputVectorTexture;
+        outputVectorTexture = tmp;
+    }
+
+    gl.readPixels(0, 0, 512, 2, gl.RGBA_INTEGER, gl.INT, output);
+    console.log(output.filter((_val, i) => i % 4 == 0));
+
+    await new Promise(resolve => setTimeout(resolve, 0));
+    end = performance.now();
+    console.log('duration: ' + (end - start))
 }
 
-async function createInputDataTexture(canvas, gl) {
-    const response = fetch("mock-matrix.bin");
-    const buf = await (await response).arrayBuffer();
+class GlProgram {
+    constructor(gl, vertexShader, fragmentShaderId, textureNames) {
+        const fragmentShader = compileShader(gl, fragmentShaderId);
+        this.program = createProgram(gl, vertexShader, fragmentShader);
+        this.textureLocations = textureNames.map(name => gl.getUniformLocation(this.program, name))
+    }
+
+    run(gl, textures, targetTexture, width, height) {
+        gl.viewport(0, 0, width, height);
+        gl.useProgram(this.program);
+
+        const gl_textures = [gl.TEXTURE0, gl.TEXTURE1, gl.TEXTURE2, gl.TEXTURE3, gl.TEXTURE4];
+        for (let i = 0; i != textures.length; ++i) {
+            gl.uniform1i(this.textureLocations[i], i);
+            gl.activeTexture(gl_textures[i]);
+            gl.bindTexture(gl.TEXTURE_2D, textures[i]);
+        }
+
+        gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, targetTexture, 0);
+
+        initVertexBuffers(gl, this.program); // TODO: is this necessary each time?
+        gl.drawArrays(gl.TRIANGLES, 0, 6);
+    }
+}
+
+function createEmptyLookupTableTexture(gl) {
+    const texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+
+    // Upload the (still empty) texture to the GPU:
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.R8UI, 512, 8, 0, gl.RED_INTEGER, gl.UNSIGNED_BYTE, null);
+
+    // can't filter integer textures
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+
+    return texture;
+}
+
+
+async function createInputDataTexture(gl) {
+    const response = await fetch("mock-matrix.bin");
+    const buf = await response.arrayBuffer();
     const data = new Uint16Array(buf);
 
     const texture = gl.createTexture();
@@ -30,132 +117,19 @@ async function createInputDataTexture(canvas, gl) {
     return texture;
 }
 
-async function createLookupTable(canvas, gl, inputDataTexture) {
-    // BEGIN SETUP (has to be done only once)
-    const vertexShader = compileShader(gl, "vertex-shader");
-    const fragmentShader = compileShader(gl, "create-lookup-shader");
-    const program = createProgram(gl, vertexShader, fragmentShader);
+function createVectorTexture(gl) {
+    const data = new Int16Array(Array.from({ length: 1024 }, (_, i) => i));
+    const texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    // Upload the texture to the GPU:
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.R16I, 512, 2, 0, gl.RED_INTEGER, gl.SHORT, data);
 
-    const compressedDataLocation = gl.getUniformLocation(program, "compressedData");
-    const lookupTableLocation = gl.getUniformLocation(program, "lookupTable");
-    // END SETUP
-
-    canvas.height = 8;
-    await new Promise(setTimeout)
-    gl.viewport(0, 0, 512, 8);
-
-    gl.useProgram(program);
-    // set which texture units to render with.
-    gl.uniform1i(compressedDataLocation, 0);  // texture unit 0
-    gl.uniform1i(lookupTableLocation, 1);  // texture unit 1
-
-    // Set each texture unit to use a particular texture.
-    gl.activeTexture(gl.TEXTURE0);
-    gl.bindTexture(gl.TEXTURE_2D, inputDataTexture);
-    gl.activeTexture(gl.TEXTURE1);
-    const targetTexture = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_2D, targetTexture);
-
-    const level = 0;
-    const internalFormat = gl.R8UI;
-    const border = 0;
-    const format = gl.RED_INTEGER;
-    const type = gl.UNSIGNED_BYTE;
-    // Upload the (still empty) texture to the GPU:
-    gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
-        512, 8, border,
-        format, type, null);
-
+    // can't filter integer textures
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
 
-    const framebuffer = gl.createFramebuffer();
-    gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer);
-
-    gl.framebufferTexture2D(
-        gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, targetTexture, level);
-
-
-    let num_vertices = initVertexBuffers(gl, program);
-    gl.drawArrays(gl.TRIANGLES, 0, num_vertices);
-
-    // let rawBuffer = new ArrayBuffer(512 * 8 * 4 * 4);
-    // let output = new Uint32Array(rawBuffer);
-    // gl.readPixels(0, 0, 512, 8, gl.RGBA_INTEGER, gl.UNSIGNED_INT, output);
-    // console.log(output.filter((_val, i) => i % 4 == 0));
-
-    return targetTexture;
-};
-
-async function decodeMatrix(canvas, gl, inputDataTexture, lookupTableTexture) {
-    // BEGIN SETUP (has to be done only once)
-    const vertexShader = compileShader(gl, "vertex-shader");
-    const fragmentShader = compileShader(gl, "decode-shader");
-    const program = createProgram(gl, vertexShader, fragmentShader);
-
-    const compressedDataLocation = gl.getUniformLocation(program, "compressedData");
-    const lookupTableLocation = gl.getUniformLocation(program, "lookupTable");
-    const saltLocation = gl.getUniformLocation(program, "salt");
-    // END SETUP
-
-    canvas.height = 2;
-    await new Promise(setTimeout)
-    gl.viewport(0, 0, 512, 2);
-
-    gl.useProgram(program);
-    // set which texture units to render with.
-    gl.uniform1i(compressedDataLocation, 0);  // texture unit 0
-    gl.uniform1i(lookupTableLocation, 1);  // texture unit 1
-
-    // Upload existing textures (TODO: is this necessary again?)
-    gl.activeTexture(gl.TEXTURE0);
-    gl.bindTexture(gl.TEXTURE_2D, inputDataTexture);
-    gl.activeTexture(gl.TEXTURE1);
-    gl.bindTexture(gl.TEXTURE_2D, lookupTableTexture);
-
-    // Bind new texture.
-    gl.activeTexture(gl.TEXTURE2);
-    const targetTexture = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_2D, targetTexture);
-
-    const level = 0;
-    const internalFormat = gl.R32UI;
-    const border = 0;
-    const format = gl.RED_INTEGER;
-    const type = gl.UNSIGNED_INT;
-    // Upload the (still empty) texture to the GPU:
-    gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
-        512, 2, border, format, type, null);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-
-    const framebuffer = gl.createFramebuffer();
-    gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer);
-    gl.framebufferTexture2D(
-        gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, targetTexture, level);
-
-    let num_vertices = initVertexBuffers(gl, program);
-
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-    console.log("start");
-    await new Promise((resolve) => setTimeout(resolve, 500));
-
-    const start = new Date();
-    for (let i = 0; i != 10000; ++i) {
-        gl.uniform1ui(saltLocation, i);
-        gl.drawArrays(gl.TRIANGLES, 0, num_vertices);
-    }
-    const end = new Date();
-    console.log("duration: " + (end - start))
-
-    let rawBuffer = new ArrayBuffer(1024 * 4 * 4);
-    let output = new Uint32Array(rawBuffer);
-    gl.readPixels(0, 0, 512, 2, gl.RGBA_INTEGER, gl.UNSIGNED_INT, output);
-    console.log(output.filter((_val, i) => i % 4 == 0));
-    document.write(output.filter((_val, i) => i % 4 == 0));
-
-    return targetTexture;
-};
+    return texture;
+}
 
 function initVertexBuffers(gl, program) {
     var vertices = new Float32Array([
