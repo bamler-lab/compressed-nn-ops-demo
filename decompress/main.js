842b77097dc46a27360cc9c5cedeb1cb5d01bc74
Robert Bamler <robert.bamler@uni-tuebingen.de>
2024-04-24 14:52:09 +0200

    Decoding first matrix works, summing up offsets seems to work too

--- decompress/main.js
+++ decompress/main.js
@@ -7,121 +7,60 @@ async function run() {
     const buf = await response.arrayBuffer();
     const data = new Uint16Array(buf);
 
-    const coderHeads = createTexture(gl, gl.RG16UI, 1024, 1024, gl.RG_INTEGER, gl.UNSIGNED_SHORT, data.subarray(0, 1024 * 1024 * 2));
-    const compressedData = createTexture(gl, gl.R16UI, 1024, 1024, gl.RED_INTEGER, gl.UNSIGNED_SHORT, data.subarray(1024 * 1024 * 2, 1024 * 1024 * 3));
-    const lookupTable = createTexture(gl, gl.R16UI, 4096, 1, gl.RED_INTEGER, gl.BYTE, null);
+    const coderHeadsIn = createTexture(gl, gl.RG16UI, 1024, 1024, gl.RG_INTEGER, gl.UNSIGNED_SHORT, data.subarray(0, 1024 * 1024 * 2));
+    const coderHeadsOut = createTexture(gl, gl.RG16UI, 1024, 1024, gl.RG_INTEGER, gl.UNSIGNED_SHORT, null);
+    const compressedData = createTexture(gl, gl.R16UI, 1024, 1024, gl.RED_INTEGER, gl.UNSIGNED_SHORT, data.subarray(1 + 2 * 1024 * 1024, 1 + 3 * 1024 * 1024));
+    const lookupTable = createTexture(gl, gl.R8UI, 4096, 1, gl.RED_INTEGER, gl.UNSIGNED_BYTE, null);
 
-    const offsetTexture = createOffsetTexture(gl);
-    const partpartsumsTexture = createEmptySumsTexture(gl, 1024, 1024);
-    const partsumsTexture = createEmptySumsTexture(gl, 128, 1024);
-    const rowsumsTexture = createEmptySumsTexture(gl, 32, 1024);
-    const sumsumspartTexture = createEmptySumsTexture(gl, 1, 1024);
-    const sumsumsTexture = createEmptySumsTexture(gl, 1, 32);
+    const coderOffsets1 = createTexture(gl, gl.R8UI, 1024, 1024, gl.RED_INTEGER, gl.UNSIGNED_BYTE, null);
+    const coderOffsets2 = createTexture(gl, gl.R8UI, 256, 1024, gl.RED_INTEGER, gl.UNSIGNED_BYTE, null);
+    const coderOffsets3 = createTexture(gl, gl.R16UI, 32, 1024, gl.RED_INTEGER, gl.UNSIGNED_SHORT, null);
+    const coderOffsets4 = createTexture(gl, gl.R16UI, 1, 1024, gl.RED_INTEGER, gl.UNSIGNED_SHORT, null);
+    const coderOffsets5 = createTexture(gl, gl.R32UI, 1, 32, gl.RED_INTEGER, gl.UNSIGNED_INT, null);
 
-    let { vertexBuffer: vertexBuffer1, vertices: vertices1 } = createVertexBuffer(gl);
-    const framebuffer1 = gl.createFramebuffer();
-    gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer1);
+    const matrix0 = createTexture(gl, gl.R16UI, 1024, 1024, gl.RED_INTEGER, gl.UNSIGNED_SHORT, null);
 
-    const createPartpartsums = new GlProgram(gl, vertexShader, "partpartsum-shader", ["input_data"]);
-    const createPartsums = new GlProgram(gl, vertexShader, "partsum-shader", ["input_data"]);
-    const createRowsums = new GlProgram(gl, vertexShader, "rowsum-shader", ["input_data"]);
-    const createSumsumspart = new GlProgram(gl, vertexShader, "sumsumpart-shader", ["input_data"]);
-    const createSumsums = new GlProgram(gl, vertexShader, "sumsum-shader", ["input_data"]);
-
-    await new Promise(resolve => setTimeout(resolve, 1000));
-
-    let start1 = performance.now();
-    for (let i = 0; i != 1000; ++i) {
-        createPartpartsums.run(gl, [offsetTexture], partpartsumsTexture, 1024, 1024, vertexBuffer1, vertices1);
-        createPartsums.run(gl, [partpartsumsTexture], partsumsTexture, 128, 1024, vertexBuffer1, vertices1);
-        createRowsums.run(gl, [partsumsTexture], rowsumsTexture, 32, 1024, vertexBuffer1, vertices1);
-        createSumsumspart.run(gl, [rowsumsTexture], sumsumspartTexture, 1, 1024, vertexBuffer1, vertices1);
-        createSumsums.run(gl, [sumsumspartTexture], sumsumsTexture, 1, 32, vertexBuffer1, vertices1);
-    }
-
-    rawBuffer1 = new ArrayBuffer(32 * 1 * 4 * 4);
-    output1 = new Uint32Array(rawBuffer1);
-    gl.readPixels(0, 0, 1, 32, gl.RGBA_INTEGER, gl.UNSIGNED_INT, output1);
-    console.log(output1.filter((_val, i) => i % 4 == 0));
-
-    await new Promise(resolve => setTimeout(resolve, 0));
-    let end1 = performance.now();
-    console.log('duration: ' + (end1 - start1))
-
-    await new Promise(resolve => setTimeout(resolve, 1000));
-
-    start1 = performance.now();
-    for (let i = 0; i != 1000; ++i) {
-        createPartpartsums.run(gl, [offsetTexture], partpartsumsTexture, 1024, 1024, vertexBuffer1, vertices1);
-        createPartsums.run(gl, [partpartsumsTexture], partsumsTexture, 128, 1024, vertexBuffer1, vertices1);
-        createRowsums.run(gl, [partsumsTexture], rowsumsTexture, 32, 1024, vertexBuffer1, vertices1);
-        createSumsumspart.run(gl, [rowsumsTexture], sumsumspartTexture, 1, 1024, vertexBuffer1, vertices1);
-        createSumsums.run(gl, [sumsumspartTexture], sumsumsTexture, 1, 32, vertexBuffer1, vertices1);
-    }
-
-    rawBuffer1 = new ArrayBuffer(32 * 1 * 4 * 4);
-    output1 = new Uint32Array(rawBuffer1);
-    gl.readPixels(0, 0, 1, 32, gl.RGBA_INTEGER, gl.UNSIGNED_INT, output1);
-    console.log(output1.filter((_val, i) => i % 4 == 0));
-
-    await new Promise(resolve => setTimeout(resolve, 0));
-    end1 = performance.now();
-    console.log('duration: ' + (end1 - start1))
-
-    return;
-
-    const compressedMatrixTexture = await createInputDataTexture(gl);
-    let inputVectorTexture = createVectorTexture(gl);
-    let outputVectorTexture = createVectorTexture(gl);
     let { vertexBuffer, vertices } = createVertexBuffer(gl);
-
     const framebuffer = gl.createFramebuffer();
     gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer);
 
-    const createLookupTable = new GlProgram(gl, vertexShader, "create-lookup-shader", ["compressedMatrix"]);
-    const compressedMatrixVectorProduct = new GlProgram(
-        gl, vertexShader, "compressed-mat-vec-mul-shader", ["compressedMatrix", "lookupTable", "vector"]);
+    const createLookup = new GlProgram(gl, vertexShader, "create-lookup-shader", ["compressedData"]);
+    const decode = new GlProgram(gl, vertexShader, "decode-shader", ["coderHeads", "compressedData", "lookupTable", "coderOffsets1", "coderOffsets2", "coderOffsets3", "coderOffsets4", "coderOffsets5"]);
 
-    await new Promise(resolve => setTimeout(resolve, 1000));
+    const sumOffsets0 = new GlProgram(gl, vertexShader, "sumOffsets0-shader", ["coderHeads"]);
+    const sumOffsets1 = new GlProgram(gl, vertexShader, "sumOffsets1-shader", ["sumOffsets0"]);
+    const sumOffsets2 = new GlProgram(gl, vertexShader, "sumOffsets2-shader", ["sumOffsets1"]);
+    const sumOffsets3 = new GlProgram(gl, vertexShader, "sumOffsets3-shader", ["sumOffsets2"]);
+    const sumOffsets4 = new GlProgram(gl, vertexShader, "sumOffsets4-shader", ["sumOffsets3"]);
+
+    // await new Promise(resolve => setTimeout(resolve, 1000));
 
     let start = performance.now();
-    for (let i = 0; i != 1000; ++i) {
-        createLookupTable.run(gl, [compressedMatrixTexture], lookupTableTexture, 4096, 1, vertexBuffer, vertices);
-        compressedMatrixVectorProduct.run(
-            gl, [compressedMatrixTexture, lookupTableTexture, inputVectorTexture],
-            outputVectorTexture, 1024, 1, vertexBuffer, vertices);
-        let tmp = inputVectorTexture;
-        inputVectorTexture = outputVectorTexture;
-        outputVectorTexture = tmp;
+    for (let i = 0; i != 1; ++i) {
+        createLookup.run(gl, [compressedData], [lookupTable], 4096, 1, vertexBuffer, vertices);
+        decode.run(gl, [coderHeadsIn, compressedData, lookupTable, coderOffsets1, coderOffsets2, coderOffsets3, coderOffsets4, coderOffsets5], [matrix0, coderHeadsOut], 1024, 1024, vertexBuffer, vertices);
+        sumOffsets0.run(gl, [coderHeadsOut], [coderOffsets1], 1024, 1024, vertexBuffer, vertices);
+        sumOffsets1.run(gl, [coderOffsets1], [coderOffsets2], 256, 1024, vertexBuffer, vertices);
+        sumOffsets2.run(gl, [coderOffsets2], [coderOffsets3], 32, 1024, vertexBuffer, vertices);
+        sumOffsets3.run(gl, [coderOffsets3], [coderOffsets4], 1, 1024, vertexBuffer, vertices);
+        sumOffsets4.run(gl, [coderOffsets4], [coderOffsets5], 1, 32, vertexBuffer, vertices);
     }
 
-    let rawBuffer = new ArrayBuffer(1024 * 1 * 4 * 4);
-    let output = new Int32Array(rawBuffer);
-    gl.readPixels(0, 0, 1024, 1, gl.RGBA_INTEGER, gl.INT, output);
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, coderOffsets5, 0);
+    let rawBuffer = new ArrayBuffer(1 * 32 * 4 * 4);
+    let output = new Uint32Array(rawBuffer);
+    gl.readPixels(0, 0, 1, 32, gl.RGBA_INTEGER, gl.UNSIGNED_INT, output);
+    console.log(output.filter((_val, i) => i % 4 == 0));
+    console.log({ sum: output.filter((_val, i) => i % 4 == 0).reduce((s, x) => s + x, 0) });
+
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, matrix0, 0);
+    rawBuffer = new ArrayBuffer(1024 * 1024 * 4 * 4);
+    output = new Uint32Array(rawBuffer);
+    gl.readPixels(0, 0, 1024, 1024, gl.RGBA_INTEGER, gl.UNSIGNED_INT, output);
     console.log(output.filter((_val, i) => i % 4 == 0));
 
     await new Promise(resolve => setTimeout(resolve, 0));
     let end = performance.now();
-    console.log('duration: ' + (end - start))
-
-    await new Promise(resolve => setTimeout(resolve, 1000));
-
-    start = performance.now();
-    for (let i = 0; i != 1000; ++i) {
-        createLookupTable.run(gl, [compressedMatrixTexture], lookupTableTexture, 4096, 1, vertexBuffer, vertices);
-        compressedMatrixVectorProduct.run(
-            gl, [compressedMatrixTexture, lookupTableTexture, inputVectorTexture],
-            outputVectorTexture, 1024, 1, vertexBuffer, vertices);
-        let tmp = inputVectorTexture;
-        inputVectorTexture = outputVectorTexture;
-        outputVectorTexture = tmp;
-    }
-
-    gl.readPixels(0, 0, 1024, 1, gl.RGBA_INTEGER, gl.INT, output);
-    console.log(output.filter((_val, i) => i % 4 == 0));
-
-    await new Promise(resolve => setTimeout(resolve, 0));
-    end = performance.now();
     console.log('duration: ' + (end - start))
 }
 
@@ -149,7 +88,7 @@ class GlProgram {
         this.vertexPositionLocation = gl.getUniformLocation(this.program, "vertex_position");
     }
 
-    run(gl, textures, targetTexture, width, height, vertexBuffer, vertices) {
+    run(gl, inputTextures, outputTextures, width, height, vertexBuffer, vertices) {
         gl.viewport(0, 0, width, height);
         gl.useProgram(this.program);
 
@@ -160,16 +99,27 @@ class GlProgram {
         gl.vertexAttribPointer(this.vertexPositionLocation, 2, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(this.vertexPositionLocation);
 
-        const gl_textures = [gl.TEXTURE0, gl.TEXTURE1, gl.TEXTURE2, gl.TEXTURE3, gl.TEXTURE4];
-        for (let i = 0; i != textures.length; ++i) {
+        // WebGL is guaranteed to have at least 8 texture units.
+        const gl_textures = [gl.TEXTURE0, gl.TEXTURE1, gl.TEXTURE2, gl.TEXTURE3, gl.TEXTURE4, gl.TEXTURE5, gl.TEXTURE6, gl.TEXTURE7];
+        for (let i = 0; i != inputTextures.length; ++i) {
             gl.uniform1i(this.textureLocations[i], i);
             gl.activeTexture(gl_textures[i]);
-            gl.bindTexture(gl.TEXTURE_2D, textures[i]);
+            gl.bindTexture(gl.TEXTURE_2D, inputTextures[i]);
         }
 
-        gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, targetTexture, 0);
+        // Not sure how many color attachments we can rely on.
+        const gl_color_attachments = [gl.COLOR_ATTACHMENT0, gl.COLOR_ATTACHMENT1, gl.COLOR_ATTACHMENT2, gl.COLOR_ATTACHMENT3];
+        for (let i = 0; i != outputTextures.length; ++i) {
+            gl.framebufferTexture2D(gl.FRAMEBUFFER, gl_color_attachments[i], gl.TEXTURE_2D, outputTextures[i], 0);
+            // TODO: maybe use gl.DRAW_FRAMEBUFFER
+        }
 
+        gl.drawBuffers(gl_color_attachments.slice(0, outputTextures.length));
         gl.drawArrays(gl.TRIANGLES, 0, 6);
+
+        for (let i = 0; i != outputTextures.length; ++i) {
+            gl.framebufferTexture2D(gl.FRAMEBUFFER, gl_color_attachments[i], gl.TEXTURE_2D, null, 0);
+        }
     }
 }
 
