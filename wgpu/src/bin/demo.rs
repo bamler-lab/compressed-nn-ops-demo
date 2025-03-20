35a507d45b5336be75f2dc3d0a087991f6364dd1
Robert Bamler <robert.bamler@uni-tuebingen.de>
2025-03-20 19:21:15 +0100

    Hacky but working compute shader to create ppf

--- wgpu/src/bin/demo.rs
+++ wgpu/src/bin/demo.rs
@@ -1,20 +1,23 @@
-use std::{num::NonZeroU64, path::PathBuf, str::FromStr};
+use std::{io::Read, num::NonZeroU64, path::PathBuf};
 
+use anyhow::Result;
 use clap::Parser;
-use log::{error, info};
+use compressed_mat_vec_mul::{FileHeader, UncompressedVector};
+use log::info;
 use wgpu::util::DeviceExt;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
-    /// List of numbers to double.
-    numbers: Vec<f32>,
+    /// Path to a file containing compressed matrices and an initial vector.
+    /// Use `mk-random` to create a file with mock data in the expected format.
+    input: PathBuf,
 
     #[command(flatten)]
     verbose: clap_verbosity_flag::Verbosity,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let cli = Cli::parse();
 
     // wgpu uses `log` rather than `tracing`, so we'll use that too.
@@ -22,25 +25,13 @@ fn main() {
         .filter_level(cli.verbose.log_level_filter())
         .init();
 
-    let arguments = cli.numbers;
+    let file = std::fs::File::open(&cli.input)?;
+    let mut reader = std::io::BufReader::new(file);
 
-    if arguments.is_empty() {
-        error!("No arguments provided. Please provide a list of numbers to double.");
-        return;
-    }
+    let file_header = FileHeader::from_read(&mut reader)?;
+    let input_vector = UncompressedVector::from_read(&mut reader, file_header.dimensions[0])?;
 
-    info!("Parsed {} arguments.", arguments.len());
-
-    // We first initialize an wgpu `Instance`, which contains any "global" state wgpu needs.
-    //
-    // This is what loads the vulkan/dx12/metal/opengl libraries.
     let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor::default());
-
-    // We then create an `Adapter` which represents a physical gpu in the system. It allows
-    // us to query information about it and create a `Device` from it.
-    //
-    // This function is asynchronous in WebGPU, so request_adapter returns a future. On native/webgl
-    // the future resolves immediately, so we can block on it without harm.
     let adapter =
         pollster::block_on(instance.request_adapter(&wgpu::RequestAdapterOptions::default()))
             .expect("Failed to create adapter");
@@ -48,9 +39,20 @@ fn main() {
     // Print out some basic information about the adapter.
     info!("Running on Adapter: {:#?}", adapter.get_info());
 
-    // Check to see if the adapter supports compute shaders. While WebGPU guarantees support for
-    // compute shaders, wgpu supports a wider range of devices through the use of "downlevel" devices.
+    let limits = adapter.limits();
+
+    dbg!(limits.max_compute_workgroup_storage_size);
+    dbg!(limits.max_compute_invocations_per_workgroup);
+    dbg!(limits.max_compute_workgroup_size_x);
+    dbg!(limits.max_compute_workgroup_size_y);
+    dbg!(limits.max_compute_workgroup_size_z);
+    dbg!(limits.max_compute_workgroups_per_dimension);
+    dbg!(limits.min_subgroup_size);
+    dbg!(limits.max_subgroup_size);
+    dbg!(limits.max_push_constant_size);
+
     let downlevel_capabilities = adapter.get_downlevel_capabilities();
+    dbg!(&downlevel_capabilities);
     if !downlevel_capabilities
         .flags
         .contains(wgpu::DownlevelFlags::COMPUTE_SHADERS)
@@ -58,43 +60,72 @@ fn main() {
         panic!("Adapter does not support compute shaders");
     }
 
-    // We then create a `Device` and a `Queue` from the `Adapter`.
-    //
-    // The `Device` is used to create and manage GPU resources.
-    // The `Queue` is a queue used to submit work for the GPU to process.
     let (device, queue) = pollster::block_on(adapter.request_device(
         &wgpu::DeviceDescriptor {
             label: None,
-            required_features: wgpu::Features::empty(),
-            required_limits: wgpu::Limits::downlevel_defaults(),
-            memory_hints: wgpu::MemoryHints::MemoryUsage,
+
+            // TODO: try `PIPELINE_STATISTICS_QUERY` and `TIMESTAMP_QUERY_INSIDE_*`.
+            // TODO: try `MAPPABLE_PRIMARY_BUFFERS` (with caution)
+            // TODO (important): try `SUBGROUP` and `SUBGROUP_BARRIER`
+            required_features: wgpu::Features::SUBGROUP
+                | wgpu::Features::SUBGROUP_BARRIER
+                | wgpu::Features::SHADER_INT64,
+            required_limits: wgpu::Limits::downlevel_defaults(), // TODO: sets `{min, max}_subgroup_size` to 0
+            memory_hints: wgpu::MemoryHints::Performance,
         },
         None,
     ))
     .expect("Failed to create device");
 
-    // Create a shader module from our shader code. This will parse and validate the shader.
-    //
-    // `include_wgsl` is a macro provided by wgpu like `include_str` which constructs a ShaderModuleDescriptor.
-    // If you want to load shaders differently, you can construct the ShaderModuleDescriptor manually.
-    let module = device.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
+    dbg!(&device);
 
-    // Create a buffer with the data we want to process on the GPU.
-    //
-    // `create_buffer_init` is a utility provided by `wgpu::util::DeviceExt` which simplifies creating
-    // a buffer with some initial data.
-    //
-    // We use the `bytemuck` crate to cast the slice of f32 to a &[u8] to be uploaded to the GPU.
-    let input_data_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+    // TODO: try `create_shader_module_trusted` and turn off unnecessary runtime checks.
+    let mut shader_file = std::fs::File::open("src/shader.wgsl")?;
+    let mut shader_code = String::new();
+    shader_file.read_to_string(&mut shader_code)?;
+    // let module = device.create_shader_module(wgpu::include_wgsl!("../shader.wgsl"));
+    let module = device.create_shader_module(wgpu::ShaderModuleDescriptor {
         label: None,
-        contents: bytemuck::cast_slice(&arguments),
+        source: wgpu::ShaderSource::Wgsl(shader_code.into()),
+    });
+
+    let globals = [
+        0,                                // cursor
+        file_header.dimensions[0] as u32, // input dimension
+        file_header.dimensions[1] as u32, // output dimension
+    ];
+    let globals_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: None,
+        contents: bytemuck::cast_slice(&globals),
+        usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+    });
+
+    let mut compressed_data = vec![0; file_header.max_compressed_matrix_size as usize];
+    let compressed_len =
+        (file_header.offsets_and_file_size[1] - file_header.offsets_and_file_size[0]) as usize;
+    reader.read_exact(&mut compressed_data[0..compressed_len])?;
+    let compressed_data_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: None,
+        contents: &compressed_data[0..compressed_len],
         usage: wgpu::BufferUsages::STORAGE,
     });
 
-    // Now we create a buffer to store the output data.
-    let output_data_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+    let input_vector_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
         label: None,
-        size: input_data_buffer.size(),
+        contents: bytemuck::cast_slice(&input_vector.0),
+        usage: wgpu::BufferUsages::STORAGE,
+    });
+
+    // let ppf_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+    //     label: None,
+    //     size: header.input_vector.len() as u64,
+    //     usage: wgpu::BufferUsages::STORAGE,
+    //     mapped_at_creation: false,
+    // });
+
+    let output_vector_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+        label: None,
+        size: 4 * file_header.dimensions[1] as u64,
         usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_SRC,
         mapped_at_creation: false,
     });
@@ -104,7 +135,7 @@ fn main() {
     // and that usage can only be used with `COPY_DST`.
     let download_buffer = device.create_buffer(&wgpu::BufferDescriptor {
         label: None,
-        size: input_data_buffer.size(),
+        size: 4 * file_header.dimensions[1] as u64,
         usage: wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::MAP_READ,
         mapped_at_creation: false,
     });
@@ -115,26 +146,50 @@ fn main() {
     let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
         label: None,
         entries: &[
-            // Input buffer
+            // `globals_buffer`
             wgpu::BindGroupLayoutEntry {
                 binding: 0,
                 visibility: wgpu::ShaderStages::COMPUTE,
                 ty: wgpu::BindingType::Buffer {
-                    ty: wgpu::BufferBindingType::Storage { read_only: true },
-                    // This is the size of a single element in the buffer.
-                    min_binding_size: Some(NonZeroU64::new(4).unwrap()),
+                    ty: wgpu::BufferBindingType::Uniform,
+                    min_binding_size: Some(
+                        NonZeroU64::new((globals.len() * size_of::<u32>()) as u64).unwrap(),
+                    ),
                     has_dynamic_offset: false,
                 },
                 count: None,
             },
-            // Output buffer
+            // `input_data_buffer`
             wgpu::BindGroupLayoutEntry {
                 binding: 1,
                 visibility: wgpu::ShaderStages::COMPUTE,
                 ty: wgpu::BindingType::Buffer {
-                    ty: wgpu::BufferBindingType::Storage { read_only: false },
+                    ty: wgpu::BufferBindingType::Storage { read_only: true },
                     // This is the size of a single element in the buffer.
-                    min_binding_size: Some(NonZeroU64::new(4).unwrap()),
+                    min_binding_size: Some(NonZeroU64::new(size_of::<u32>() as u64).unwrap()), // TODO: what is this for?
+                    has_dynamic_offset: false, // TODO: can we use this?
+                },
+                count: None,
+            },
+            // `input_vector_buffer`
+            wgpu::BindGroupLayoutEntry {
+                binding: 2,
+                visibility: wgpu::ShaderStages::COMPUTE,
+                ty: wgpu::BindingType::Buffer {
+                    ty: wgpu::BufferBindingType::Storage { read_only: true },
+                    // This is the size of a single element in the buffer.
+                    min_binding_size: Some(NonZeroU64::new(4).unwrap()), // TODO: what is this for?
+                    has_dynamic_offset: false,
+                },
+                count: None,
+            },
+            // `output_vector_buffer`
+            wgpu::BindGroupLayoutEntry {
+                binding: 3,
+                visibility: wgpu::ShaderStages::COMPUTE,
+                ty: wgpu::BindingType::Buffer {
+                    ty: wgpu::BufferBindingType::Storage { read_only: false },
+                    min_binding_size: Some(NonZeroU64::new(4).unwrap()), // TODO: what is this for?
                     has_dynamic_offset: false,
                 },
                 count: None,
@@ -152,11 +207,19 @@ fn main() {
         entries: &[
             wgpu::BindGroupEntry {
                 binding: 0,
-                resource: input_data_buffer.as_entire_binding(),
+                resource: globals_buffer.as_entire_binding(),
             },
             wgpu::BindGroupEntry {
                 binding: 1,
-                resource: output_data_buffer.as_entire_binding(),
+                resource: compressed_data_buffer.as_entire_binding(), // TODO: try `.slice()` instead.
+            },
+            wgpu::BindGroupEntry {
+                binding: 2,
+                resource: input_vector_buffer.as_entire_binding(),
+            },
+            wgpu::BindGroupEntry {
+                binding: 3,
+                resource: output_vector_buffer.as_entire_binding(),
             },
         ],
     });
@@ -165,7 +228,7 @@ fn main() {
     let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
         label: None,
         bind_group_layouts: &[&bind_group_layout],
-        push_constant_ranges: &[],
+        push_constant_ranges: &[], // TODO
     });
 
     // The pipeline is the ready-to-go program state for the GPU. It contains the shader modules,
@@ -174,9 +237,9 @@ fn main() {
         label: None,
         layout: Some(&pipeline_layout),
         module: &module,
-        entry_point: Some("doubleMe"),
-        compilation_options: wgpu::PipelineCompilationOptions::default(),
-        cache: None,
+        entry_point: Some("mat_vec_mul"),
+        compilation_options: wgpu::PipelineCompilationOptions::default(), // TODO: set `zero_initialize_workgroup_memory = false`
+        cache: None,                                                      // TODO
     });
 
     // The command encoder allows us to record commands that we will later submit to the GPU.
@@ -195,13 +258,9 @@ fn main() {
     // Set the bind group that we want to use
     compute_pass.set_bind_group(0, &bind_group, &[]);
 
-    // Now we dispatch a series of workgroups. Each workgroup is a 3D grid of individual programs.
-    //
-    // We defined the workgroup size in the shader as 64x1x1. So in order to process all of our
-    // inputs, we ceiling divide the number of inputs by 64. If the user passes 32 inputs, we will
-    // dispatch 1 workgroups. If the user passes 65 inputs, we will dispatch 2 workgroups, etc.
-    let workgroup_count = arguments.len().div_ceil(64);
-    compute_pass.dispatch_workgroups(workgroup_count as u32, 1, 1);
+    const WORKGROUP_SIZE: u32 = 64;
+    let workgroup_count = file_header.dimensions[1].div_ceil(WORKGROUP_SIZE);
+    compute_pass.dispatch_workgroups(workgroup_count, 1, 1);
 
     // Now we drop the compute pass, giving us access to the encoder again.
     drop(compute_pass);
@@ -209,11 +268,11 @@ fn main() {
     // We add a copy operation to the encoder. This will copy the data from the output buffer on the
     // GPU to the download buffer on the CPU.
     encoder.copy_buffer_to_buffer(
-        &output_data_buffer,
+        &output_vector_buffer,
         0,
         &download_buffer,
         0,
-        output_data_buffer.size(),
+        output_vector_buffer.size(),
     );
 
     // We finish the encoder, giving us a fully recorded command buffer.
@@ -235,6 +294,7 @@ fn main() {
     buffer_slice.map_async(wgpu::MapMode::Read, |_| {
         // In this case we know exactly when the mapping will be finished,
         // so we don't need to do anything in the callback.
+        // TODO: what does this mean?
     });
 
     // Wait for the GPU to finish working on the submitted work. This doesn't work on WebGPU, so we would need
@@ -243,9 +303,10 @@ fn main() {
 
     // We can now read the data from the buffer.
     let data = buffer_slice.get_mapped_range();
-    // Convert the data back to a slice of f32.
-    let result: &[f32] = bytemuck::cast_slice(&data);
+    let result: &[i32] = bytemuck::cast_slice(&data);
 
     // Print out the result.
     println!("Result: {:?}", result);
+
+    Ok(())
 }
--- wgpu/src/shader.wgsl
+++ wgpu/src/shader.wgsl
@@ -1,27 +1,138 @@
-// Input to the shader. The length of the array is determined by what buffer is bound.
-//
-// Out of bounds accesses 
+struct Globals {
+  cursor: u32,
+  input_dim: u32,
+  output_dim: u32,
+}
+
 @group(0) @binding(0)
-var<storage, read> input: array<f32>;
-// Output of the shader.  
+var<uniform> globals: Globals;
+
 @group(0) @binding(1)
-var<storage, read_write> output: array<f32>;
+var<storage, read> compressed_data: array<u32>;
+
+@group(0) @binding(2)
+var<storage, read> input_vector: array<i32>;
+
+@group(0) @binding(3)
+var<storage, read_write> output_vector: array<i32>;
+
+var<workgroup> ppf: array<u32, 256>;
+
+// TODO:
+// - use a 64 bit state and decode 4 values at a time
+// - unpack the lowest 32 bit into 4 quantiles (using `unpack4xU8`), perform lookups in parallel if possible
+// - calculate state update as much as possible in 32 bit, require only one 64 bit MAC
+// - use `dot4I8Packed` to compute the dot product of length-4 slices
+// - then use `subgroupAdd` to sum the results of the dot products across a subgroup
+
+// LATER:
+// - interleave compressed data within each subgroup and use `subgroupInclusiveAdd` to calculate cursor increments
 
 // Ideal workgroup size depends on the hardware, the workload, and other factors. However, it should
 // _generally_ be a multiple of 64. Common sizes are 64x1x1, 256x1x1; or 8x8x1, 16x16x1 for 2D workloads.
 @compute @workgroup_size(64)
-fn doubleMe(@builtin(global_invocation_id) global_id: vec3<u32>) {
-    // While compute invocations are 3d, we're only using one dimension.
-    let index = global_id.x;
+fn mat_vec_mul(
+    @builtin(global_invocation_id) global_id: vec3<u32>,
+    @builtin(local_invocation_id) local_id: vec3<u32>,
+    @builtin(subgroup_id) subgroup_id: u32,
+    @builtin(subgroup_invocation_id) subgroup_invocation_id: u32,
+    @builtin(subgroup_size) subgroup_size: u32
+) {
+    // `global_invocation_id.x` ranges from 0 to 4095
+    // `subgroup_id` is [16 times 0, 16 times 1, 16 times 2, 16times 3, then repeats: 16 times 0 ...]
+    // --> probably identifies the subgroup id *within* the workgroup, where each workgroup contains 4 subgroups.
+    // --> on work PC, it's just 0.
+    // `subgroup_invocation_id` is [0, 1, 2, ..., 15, then repeats]
+    // --> on work PC, it's [0, 1, 2, ..., 63, then repeats]
+    // `subgroup_size` is 16
+    // --> on work PC, it's 64
 
-    // Because we're using a workgroup size of 64, if the input size isn't a multiple of 64,
-    // we will have some "extra" invocations. This is fine, but we should tell them to stop
-    // to avoid out-of-bounds accesses.
-    let array_length = arrayLength(&input);
-    if (global_id.x >= array_length) {
+    // Create PPF lookup table in workgroup memory. Each instance within a workgroup
+    // is responsible for filling entries ppf[(4 * local_id.x)..(4 * local_id.x + 4)].
+
+    var quantile = 4 * local_id.x;
+    var cursor = globals.cursor;
+    var word = compressed_data[cursor];
+    let grid_start = i32(((word >> 16) + 128) & 0xFF) - 128;
+    let grid_size = (word >> 24) & 0xFF;
+    cursor += 1;
+    // The left-sided cdf ranges from `compressed_data[cursor][0]` (inclusively) to 
+    // `compressed_data[cursor + grid_size / 4][grid_size % 4]` (exclusively), where
+    // the second `[]` denotes zero-based indexing of the byte in a 4-byte word.
+
+    // TODO: Maybe copy `cdf` into workgroup memory first (can be written to `ppf`
+    // buffer and then overwritten after a workgroup barrier).
+
+    // Find the last cdf entry that is <= quantile:
+    // Assume that grid_size <= 128.
+    let max_search_idx = (grid_size - 1u) / 4;
+    var search_idx = 0u;
+    var bit = 16u;
+
+    for (var i = 0u; i!=5; i+=1) {
+        let test_cdf = compressed_data[cursor + (search_idx | bit)] & 0xFF;
+        let is_leq = (test_cdf <= quantile) && ((search_idx | bit) <= max_search_idx);
+        search_idx |= bit * u32(is_leq);
+        bit >>= 1;
+    }
+
+    word = compressed_data[cursor + search_idx];
+    var test_cdf = (word >> 16) & 0xFF;
+    var is_leq = (test_cdf <= quantile) && (((search_idx * 4) | 2) < grid_size);
+    var sub_search_index = 2 * u32(is_leq);
+    
+    test_cdf = (word >> ((sub_search_index | 1) * 8)) & 0xFF;
+    is_leq = (test_cdf <= quantile) && (((search_idx * 4) | sub_search_index | 1) < grid_size);
+    sub_search_index |= u32(is_leq);
+
+    var cdf_cursor = (search_idx * 4) | sub_search_index;
+    var left_cdf = (compressed_data[cursor + cdf_cursor / 4] >> (8 * (cdf_cursor % 4))) & 0xFF;
+    cdf_cursor += 1;
+    var right_cdf = (compressed_data[cursor + cdf_cursor / 4] >> (8 * (cdf_cursor % 4))) & 0xFF;
+    var ppf_entry = pack4xU8(vec4(
+        left_cdf,
+        right_cdf - left_cdf, // `pack4xU8` will truncate this to 8 bits
+        cdf_cursor - 1, // TODO: add `grid_start` (also below)
+        0
+    ));
+    ppf[quantile] = ppf_entry;
+
+    let end_quantile = 4 * local_id.x + 3;
+    while (quantile != end_quantile) {
+        quantile += 1;
+        if (right_cdf == quantile) {
+            cdf_cursor += 1;
+            left_cdf = right_cdf;
+            right_cdf = (compressed_data[cursor + cdf_cursor / 4] >> (8 * (cdf_cursor % 4))) & 0xFF;
+            ppf_entry = pack4xU8(vec4(
+                left_cdf,
+                right_cdf - left_cdf, // `pack4xU8` will truncate this to 8 bits
+                cdf_cursor - 1,
+                0
+            ));
+        }
+        ppf[quantile] = ppf_entry;
+    }
+    
+
+    let num_rows = arrayLength(&output_vector);
+    if (global_id.x >= num_rows) {
         return;
     }
 
-    // Do the multiply by two and write to the output.
-    output[global_id.x] = input[global_id.x] * 2.0;
+    // var<workgroup> ppf: array<u32, 256>;
+
+    if (global_id.x >= globals.output_dim - 64) {
+        output_vector[globals.output_dim - 256 + 4 * local_id.x] = i32((ppf[4 * local_id.x] >> 16) & 0xFF);
+        output_vector[globals.output_dim - 256 + 4 * local_id.x + 1] = i32((ppf[4 * local_id.x + 1] >> 16) & 0xFF);
+        output_vector[globals.output_dim - 256 + 4 * local_id.x + 2] = i32((ppf[4 * local_id.x + 2] >> 16) & 0xFF);
+        output_vector[globals.output_dim - 256 + 4 * local_id.x + 3] = i32((ppf[4 * local_id.x + 3] >> 16) & 0xFF);
+        // output_vector[global_id.x] = i32((compressed_data[cursor + search_idx] >> (8 * sub_search_index)) & 0xFF);
+    } else if (global_id.x < globals.output_dim - 256) {
+        output_vector[global_id.x] = i32((compressed_data[cursor + 8] / 16) &0xff);
+    }
+
+    // output_vector[global_id.x] = i32(
+    //     (compressed_data[cursor + global_id.x / 4] >> ((global_id.x % 4) * 8)) & 0xff
+    // );
 }
